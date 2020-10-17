7c39d408fa29d0d1a89efc379659936cead0c18c
Emmanuel <ogasule001@gmail.com>
2020-10-17 23:02:46 +0100

    deleted old api-key, refactored the Nba.baseUrl attribute

--- lib/api/urls.dart
+++ lib/api/urls.dart
@@ -2,11 +2,11 @@ import 'package:hoop/api/apikey/key.dart'; // get your own nba-Api key
 
 class Urls {
   static String eastStandingsUrl =
-      "${NbaApi.baseUrl}/standings/standard/2019/conference/east/?rapidapi-key=${NbaApi.key}";
+      "https://api-nba-v1.p.rapidapi.com/standings/standard/2019/conference/east/?rapidapi-key=${NbaApi.key}";
   static String westStandingsUrl =
-      "${NbaApi.baseUrl}/standings/standard/2019/conference/west/?rapidapi-key=${NbaApi.key}";
+      "https://api-nba-v1.p.rapidapi.com/standings/standard/2019/conference/west/?rapidapi-key=${NbaApi.key}";
   static String eastTeamsUrl =
-      "${NbaApi.baseUrl}/teams/confName/east/?rapidapi-key=${NbaApi.key}";
+      "https://api-nba-v1.p.rapidapi.com/teams/confName/east/?rapidapi-key=${NbaApi.key}";
   static String westTeamsUrl =
-      "${NbaApi.baseUrl}/teams/confName/west/?rapidapi-key=${NbaApi.key}";
+      "https://api-nba-v1.p.rapidapi.com/teams/confName/west/?rapidapi-key=${NbaApi.key}";
 }
