5fc04a2846e341a3988f35ad3b2f4aafa4d85ad1
Emmanuel <ogasule001@gmail.com>
2020-11-09 16:02:00 +0100

    refactored url file, added team site functions

--- lib/services/urls.dart
+++ lib/services/urls.dart
@@ -1,15 +1,17 @@
 import 'package:hoop/services/apikey/key.dart'; // get your own nba-Api key
 
 class Urls {
+  static final String _apiBaseUrl = "https://api-nba-v1.p.rapidapi.com";
+  static final String _teamBaseUrl = "https://www.nba.com/";
   static String eastStandingsUrl =
-      "https://api-nba-v1.p.rapidapi.com/standings/standard/2019/conference/east/?rapidapi-key=${NbaApi.key}";
+      "$_apiBaseUrl/standings/standard/2019/conference/east/?rapidapi-key=${NbaApi.key}";
   static String westStandingsUrl =
-      "https://api-nba-v1.p.rapidapi.com/standings/standard/2019/conference/west/?rapidapi-key=${NbaApi.key}";
+      "$_apiBaseUrl/standings/standard/2019/conference/west/?rapidapi-key=${NbaApi.key}";
   static String eastTeamsUrl =
-      "https://api-nba-v1.p.rapidapi.com/teams/confName/east/?rapidapi-key=${NbaApi.key}";
+      "$_apiBaseUrl/teams/confName/east/?rapidapi-key=${NbaApi.key}";
   static String westTeamsUrl =
-      "https://api-nba-v1.p.rapidapi.com/teams/confName/west/?rapidapi-key=${NbaApi.key}";
-  static String teamPlayersUrl(String id) {
-    return "https://api-nba-v1.p.rapidapi.com/players/teamId/$id/?rapidapi-key=${NbaApi.key}";
-  }
+      "$_apiBaseUrl/teams/confName/west/?rapidapi-key=${NbaApi.key}";
+  static String teamPlayersUrl(String id) =>
+      "$_apiBaseUrl/players/teamId/$id/?rapidapi-key=${NbaApi.key}";
+  static String link(String team) => _teamBaseUrl + team + "/";
 }
