672bbcffea93ac0e8325c7e6f8b733f6da54a861
Emmanuel <ogasule001@gmail.com>
2020-11-09 21:39:01 +0100

    link to each teams website now works via url_launcher

--- lib/screens/views/player_view/teaminfo.dart
+++ lib/screens/views/player_view/teaminfo.dart
@@ -2,19 +2,51 @@ import 'package:hoop/components/cacheimg.dart';
 import 'package:flutter/material.dart';
 import 'package:hoop/components/statcard.dart';
 import 'package:hoop/components/playerlst.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:hoop/services/urls.dart';
 
-//TODO: Add a link to each teams official website
-class TeamDetails extends StatelessWidget {
+//TODO: Refactor code to use multiprovider to handle website view
+class TeamDetails extends StatefulWidget {
   final dynamic json;
   final String teamurl;
   final String name;
   TeamDetails(
       {@required this.json, @required this.teamurl, @required this.name});
+
+  @override
+  _TeamDetailsState createState() => _TeamDetailsState();
+}
+
+class _TeamDetailsState extends State<TeamDetails> {
   @override
   Widget build(BuildContext context) {
-    final String streak = json["winStreak"] == "1"
+    Future<void> _launched;
+    final String streak = widget.json["winStreak"] == "1"
         ? "W"
         : "L"; // has the team been on a winning or losing streak
+
+    Future<void> _launchInWebViewOrVC(String url) async {
+      if (await canLaunch(url)) {
+        await launch(
+          url,
+          forceSafariVC: true,
+          forceWebView: true,
+          enableJavaScript: true,
+          headers: <String, String>{'my_header_key': 'my_header_value'},
+        );
+      } else {
+        throw 'Could not launch $url';
+      }
+    }
+
+    Widget _launchStatus(BuildContext context, AsyncSnapshot<void> snapshot) {
+      if (snapshot.hasError) {
+        return Text('Error: ${snapshot.error}');
+      } else {
+        return const Text('');
+      }
+    }
+
     return SafeArea(
       child: Scaffold(
         backgroundColor: Color(0XFFEDF1FF),
@@ -35,15 +67,20 @@ class TeamDetails extends StatelessWidget {
                       Column(
                         children: [
                           CachedLogo(
-                            url: teamurl,
+                            url: widget.teamurl,
                             radius: 45,
                           ),
                           GestureDetector(
-                            onTap: () => print("Go to team site"),
+                            onTap: () {
+                              setState(() {
+                                _launched = _launchInWebViewOrVC(
+                                    Urls.link(widget.name.toLowerCase()));
+                              });
+                            },
                             child: Row(
                               children: [
                                 Text(
-                                  name,
+                                  widget.name,
                                   style: TextStyle(
                                     color: Colors.grey,
                                   ),
@@ -63,21 +100,22 @@ class TeamDetails extends StatelessWidget {
                             children: [
                               StatCard(
                                 item1:
-                                    "Home: ${json["home"]["win"]} - ${json["home"]["loss"]}",
+                                    "Home: ${widget.json["home"]["win"]} - ${widget.json["home"]["loss"]}",
                                 item2:
-                                    "Road: ${json["away"]["win"]} - ${json["home"]["loss"]}",
+                                    "Road: ${widget.json["away"]["win"]} - ${widget.json["home"]["loss"]}",
                                 item3:
-                                    "Last 10: ${json["lastTenWin"]} - ${json["lastTenLoss"]}",
-                                item4: "Streak: $streak${json["streak"]}",
+                                    "Last 10: ${widget.json["lastTenWin"]} - ${widget.json["lastTenLoss"]}",
+                                item4:
+                                    "Streak: $streak${widget.json["streak"]}",
                               ),
                               SizedBox(
                                 width: 10,
                               ),
                               StatCard(
-                                item1: "Win: ${json["win"]}",
-                                item2: "Loss: ${json["loss"]}",
-                                item3: "Pct: ${json["winPercentage"]}%",
-                                item4: "GB: ${json["gamesBehind"]}",
+                                item1: "Win: ${widget.json["win"]}",
+                                item2: "Loss: ${widget.json["loss"]}",
+                                item3: "Pct: ${widget.json["winPercentage"]}%",
+                                item4: "GB: ${widget.json["gamesBehind"]}",
                               )
                             ],
                           ),
@@ -89,10 +127,11 @@ class TeamDetails extends StatelessWidget {
                               StatCard(
                                 item1: "Division",
                                 item2:
-                                    "${json["division"]["name"].toString().toUpperCase()}",
+                                    "${widget.json["division"]["name"].toString().toUpperCase()}",
                                 item3:
-                                    "${json["division"]["win"]} - ${json["division"]["loss"]}",
-                                item4: "Rank: ${json["division"]["rank"]}",
+                                    "${widget.json["division"]["win"]} - ${widget.json["division"]["loss"]}",
+                                item4:
+                                    "Rank: ${widget.json["division"]["rank"]}",
                               ),
                               SizedBox(
                                 width: 10,
@@ -100,10 +139,11 @@ class TeamDetails extends StatelessWidget {
                               StatCard(
                                 item1: "Conference",
                                 item2:
-                                    "${json["conference"]["name"].toString().toUpperCase()}",
+                                    "${widget.json["conference"]["name"].toString().toUpperCase()}",
                                 item3:
-                                    "${json["conference"]["win"]} - ${json["conference"]["loss"]}",
-                                item4: "Rank: ${json["conference"]["rank"]}",
+                                    "${widget.json["conference"]["win"]} - ${widget.json["conference"]["loss"]}",
+                                item4:
+                                    "Rank: ${widget.json["conference"]["rank"]}",
                               )
                             ],
                           ),
@@ -125,8 +165,11 @@ class TeamDetails extends StatelessWidget {
               ),
               //Roster()
               PlayerList(
-                teamId: json["teamId"],
-              )
+                teamId: widget.json["teamId"],
+              ),
+              FutureBuilder<void>(
+                  future: _launched,
+                  builder: _launchStatus), // launch the teams official website
             ],
           ),
         ),
